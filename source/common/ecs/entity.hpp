d146cab6a543be61f2da29818443e9143288bb6c
Haitham-AbdelKarim <haithammoabdelkarim@gmail.com>
2023-04-14 18:12:04 +0200

    Req-8: Entity, World and Camera classes

--- source/common/ecs/entity.hpp
+++ source/common/ecs/entity.hpp
@@ -35,7 +35,9 @@ namespace our {
             static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
             //TODO: (Req 8) Create an component of type T, set its "owner" to be this entity, then push it into the component's list
             // Don't forget to return a pointer to the new component
-            return nullptr;
+            T* newComponent = new T();
+            components.push_back(newComponent);
+            return newComponent;
         }
 
         // This template method searhes for a component of type T and returns a pointer to it
@@ -44,6 +46,11 @@ namespace our {
         T* getComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // Return the component you found, or return null of nothing was found.
+            for(auto itr = components.begin(); itr != components.end(); itr++){
+                if (typeid(*itr) == typeid(T)) {
+                    return (T*)*itr;
+                }
+            }
             return nullptr;
         }
 
@@ -63,6 +70,12 @@ namespace our {
         void deleteComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // If found, delete the found component and remove it from the components list
+            for(auto itr = components.begin(); itr != components.end(); itr++){
+                if (typeid(*itr) == typeid(T)) {
+                    components.remove(*itr);
+                    return;
+                }
+            }
         }
 
         // This template method searhes for a component of type T and deletes it
@@ -80,11 +93,13 @@ namespace our {
         void deleteComponent(T const* component){
             //TODO: (Req 8) Go through the components list and find the given component "component".
             // If found, delete the found component and remove it from the components list
+            components.remove(component);
         }
 
         // Since the entity owns its components, they should be deleted alongside the entity
         ~Entity(){
             //TODO: (Req 8) Delete all the components in "components".
+            components.clear();
         }
 
         // Entities should not be copyable
